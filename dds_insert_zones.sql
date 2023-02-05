79992f0f09033a6bfcce438bc7555a683b33f495
Jud1cator <yarik200027@gmail.com>
2023-02-05 14:59:56 +0300

    Create dds_insert_zones.sql

---
+++ dds_insert_zones.sql
@@ -0,0 +1,19 @@
+insert into dds.dds_zones(
+	timezone,
+	country,
+	region_name
+)
+select
+	distinct
+	geo_timezone,
+	geo_country,
+	geo_region_name
+from
+	staging.stg_events
+where
+	staging.stg_events.event_timestamp::date = '2022-09-30'::date
+on conflict (timezone, country, region_name) do update set
+	timezone = EXCLUDED.timezone,
+	country = EXCLUDED.country,
+	region_name = EXCLUDED.region_name
+;
