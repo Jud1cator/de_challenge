bd9b7896b2a8f2a250e91943952468815d63f289
Jud1cator <yarik200027@gmail.com>
2023-02-05 14:50:05 +0300

    Create dds_insert_users.sql

---
+++ dds_insert_users.sql
@@ -0,0 +1,16 @@
+insert into dds.dds_user(
+	custom_id,
+	domain_id
+)
+select
+	distinct
+	user_custom_id,
+	user_domain_id
+from
+	staging.stg_events
+where
+	staging.stg_events.event_timestamp::date = '2022-09-30'::date
+on conflict (custom_id, domain_id) do update set
+	custom_id = EXCLUDED.custom_id,
+	domain_id = EXCLUDED.domain_id
+;
